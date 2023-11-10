10cc3721cf40aa7f45b810ee47188d41ec7f7e6d
Vincent Toups <toups@email.unc.edu>
2023-11-10 21:23:17 +0000

    Added k-fold cross validation to the gbm-responders model.

--- gbm-responders.R
+++ gbm-responders.R
@@ -1,10 +1,10 @@
 library(tidyverse);
 library(gbm);
+source("util.R")
 
 eco <- read_csv("derived_data/eco-encoded.csv");
 demo <- read_csv("derived_data/demo-encoded.csv");
-data <- read_csv("derived_data/subject-changes.csv") %>%
-    select(`Pain Interferene Start`>60) %>%
+all_data <- read_csv("derived_data/subject-changes.csv") %>%
     select(`USUBJID`,`Group`,`Visit Count`) %>%
     inner_join(eco,by="USUBJID") %>%
     inner_join(demo, by="USUBJID") %>%
@@ -12,23 +12,32 @@ data <- read_csv("derived_data/subject-changes.csv") %>%
     select(-`Group`) %>%
     filter(`Visit Count`>8) %>% select(-`Visit Count`);
 
-columns <- names(data);
+columns <- names(all_data);
 bad_columns <- Filter(function(col_name){
-    length(unique(data[[col_name]])) < 2;
+    length(unique(all_data[[col_name]])) < 2;
 },columns)
 good_columns <- columns[!(columns %in% bad_columns)]
 
 
-data <- data %>% select(all_of(good_columns))
+all_data <- all_data %>% select(all_of(good_columns))
 
 f<-improved~.
 
+k = 5;
+k <- 5;
 
-train_ii <- runif(nrow(data)) < 0.5;
-train <- data %>% filter(train_ii);
-test <- data %>% filter(!train_ii);
+fold_ii <- all_data %>% group_by(improved) %>% mutate(fold=(floor(seq(0,0.999,length.out=length(Change))*k))+1) %>% ungroup() %>% pull(fold);
 
-model <- gbm(f, distribution="bernoulli", data=train %>% select(-`USUBJID`),
-             interaction.depth=2, n.trees=100);
-roc_info <- roc(predict(model, newdata=test, type='response', n.trees=100),test$improved,pts=seq(0,1,length.out=100));
-plot_roc(roc_info);
+characterization <- do.call(rbind, Map(function(i){
+    train_ii <- fold_ii != i#runif(nrow(ex_wide_time)) < 0.75;
+    train <- all_data %>% filter(train_ii);
+    test <- all_data %>% filter(!train_ii);
+
+
+    model <- gbm(f, distribution="bernoulli", data=train %>% select(-`USUBJID`),
+                 interaction.depth=3, n.trees=300);
+    
+    roc_info <- roc(predict(model, newdata=test, type='response', n.trees=100),test$improved,pts=seq(0,1,length.out=100)) %>% mutate(fold=i);
+},1:k))
+
+
