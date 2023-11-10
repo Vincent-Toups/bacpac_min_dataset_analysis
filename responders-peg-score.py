913a785cc47b57554097423816583d1c5e5005ec
Vincent Toups <toups@email.unc.edu>
2023-11-10 21:27:03 +0000

    Modified criteria for plotting PEG scores and saved out the time series for later use.

--- responders-peg-score.py
+++ responders-peg-score.py
@@ -22,8 +22,38 @@ meta_data = (pl.read_csv("derived_data/meta-data.csv")
                             .filter(pl.col("domain")=="QS")
                             .filter(pl.col("archive")=="false"));
 
+
+def coerce_to_string(df, column_name):
+    return df.with_columns(pl.col(column_name).cast(str).alias(column_name))
+
+def replace_values_with_none(df, column_name, values=['NA', '', '.']):
+    return df.with_columns(pl.when(pl.col(column_name).is_in(values)).then(None).otherwise(pl.col(column_name)).alias(column_name))
+
+def cast_to_float(df, column_name):
+    return df.with_columns(pl.col(column_name).cast(pl.Float64).alias(column_name))
+
+def trim_whitespace(df, column_name):
+    '''
+    Trims extraneous whitespace from the specified column of the dataframe.
+
+    Parameters:
+    - df: polars DataFrame
+    - column_name: str, the column to trim
+
+    Returns:
+    - A new DataFrame with the whitespace-trimmed column
+    '''
+
+    return df.with_column(pl.col(column_name).str.strip().alias(column_name))
+
+ 
 columns = u.calc_shared_columns(list(meta_data["schema"]));
-qs = pl.concat([(pl.read_csv(file, parse_dates=True).select(columns)
+qs = pl.concat([(pl.read_csv(file, parse_dates=True).filter(c('QSTESTCD')=='PEGSCORE').select(columns)
+                 .pipe(coerce_to_string, 'QSSTRESN')
+                 .pipe(replace_values_with_none, 'QSSTRESN')
+                 .pipe(trim_whitespace,'QSSTRESN')
+                 .pipe(replace_values_with_none, 'QSSTRESN')
+                 .pipe(cast_to_float, 'QSSTRESN')
                  .with_columns(c('VISITNUM').cast(pl.Int64).alias('VISITNUM'))) for file in meta_data["file"]]);
 peg_score = (qs.filter(c('QSTESTCD')=='PEGSCORE')
                      .select(['USUBJID','VISITNUM','QSSTRESN','QSDTC'])
@@ -40,7 +70,8 @@ week_counts = (peg_score
 
 
 
-sorted_visit_numbers = [str(vn) for vn in list(peg_score.groupby('Visit Number').count().sort('Visit Number')['Visit Number'])];
+sorted_visit_numbers = [str(vn) for vn in list(peg_score.groupby('Visit Number').count().sort('Visit Number')['Visit Number']) if str(vn) != 'None'];
+
 pain_int_with_nulls = (peg_score
                        .drop('Visit Date')
                        .pivot(index='USUBJID',values='Peg Score',columns='Visit Number')
@@ -83,19 +114,23 @@ change = (early_pi
                         .otherwise('???')
                         .alias('Group')))
 
-peg_score = peg_score.join(change
-                                           .drop('Visit Count'), on="USUBJID", how="inner");
+peg_score = (peg_score.join(change
+                           .drop('Visit Count'), on="USUBJID", how="inner")
+             .filter(c('Group') != '???'))
 
-((ggplot(change.filter(c('Visit Count')>=9)
+((ggplot(change.filter(c('Visit Count')>=6)
          .to_pandas(),aes('Change'))+
   geom_histogram())).save("figures/change-distribution.png");
  
 
 ((ggplot(peg_score
-         .with_columns(pl.when(c('Peg Score Start')>6).then(pl.lit(0.75)).otherwise(pl.lit(0.25)).alias('Initial Pain High'))
+         .with_columns(pl.when(c('Peg Score Start')>7).then(pl.lit(0.75)).otherwise(pl.lit(0.25)).alias('Initial Pain High'))
          .to_pandas(), aes('Visit Number','Peg Score (Smoothed)'))+
- geom_line(aes(group='USUBJID',color='Group',alpha='Initial Pain High')))
+ geom_line(aes(group='USUBJID',color='Group',alpha='Initial Pain High'))+
+  ylim(0, 10) +
+ facet_wrap("Group",nrow=3))
 .save("figures/ts-peg-score.png"))
+
 
 (peg_score
  .sort('Visit Number')
@@ -108,3 +143,5 @@ peg_score = peg_score.join(change
 pi_initial = peg_score.groupby('USUBJID')
 
 change.write_csv("derived_data/subject-changes-peg-score.csv");
+
+peg_score.write_csv("derived_data/peg_score_ts.csv");
